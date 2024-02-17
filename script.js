d2a645d55fe9f7892f34a9fdb2a8f1c716c0b1e6
krish8555 <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2024-02-17 13:35:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 for (let i = 0; i < 350; i++) {
-  let box = document.createElement(span);
+  let box = document.createElement("span");
   document.getElementById("container").appendChild(box);
 }
+
 let cursor = document.getElementById("cursor");
 window.addEventListener("mousemove", function (e) {
   cursor.style.left = e.clientX - 250 + "px";
